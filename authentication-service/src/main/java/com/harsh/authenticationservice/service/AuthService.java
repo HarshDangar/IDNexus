86bef03db039f515b2c95cce4b4006816b541d36
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-09-26 11:58:34 +0530

    feat(authentication): add register method in service

--- authentication-service/src/main/java/com/harsh/authenticationservice/service/AuthService.java
+++ authentication-service/src/main/java/com/harsh/authenticationservice/service/AuthService.java
@@ -1,5 +1,9 @@
 package com.harsh.authenticationservice.service;
 
+import com.harsh.authenticationservice.domain.dtos.RegisterRequest;
+import com.harsh.authenticationservice.domain.entities.AuthCredential;
+
 public interface AuthService {
 
+    AuthCredential register(RegisterRequest request);
 }
--- authentication-service/src/main/java/com/harsh/authenticationservice/service/Impl/AuthServiceImpl.java
+++ authentication-service/src/main/java/com/harsh/authenticationservice/service/Impl/AuthServiceImpl.java
@@ -1,11 +1,34 @@
 package com.harsh.authenticationservice.service.Impl;
 
+import com.harsh.authenticationservice.domain.dtos.RegisterRequest;
+import com.harsh.authenticationservice.domain.entities.AuthCredential;
+import com.harsh.authenticationservice.domain.entities.AuthRole;
+import com.harsh.authenticationservice.exception.UserAlreadyExistsException;
+import com.harsh.authenticationservice.repository.AuthCredentialRepository;
 import com.harsh.authenticationservice.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
+    private final AuthCredentialRepository authCredentialRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public AuthCredential register(RegisterRequest request) {
+        if (authCredentialRepository.existsByUserName(request.getUserName())) {
+            throw new UserAlreadyExistsException(request.getUserName());
+        }
+
+        AuthCredential credential = new AuthCredential();
+        credential.setEnrollmentId(request.getEnrollmentId());
+        credential.setUserName(request.getUserName());
+        credential.setPassword(passwordEncoder.encode(request.getPassword()));
+        credential.setRole(AuthRole.USER);
+
+        return authCredentialRepository.save(credential);
+    }
 }
